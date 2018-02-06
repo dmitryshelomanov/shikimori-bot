cf31fa1ee3ac9f970fbd3f5ba0f753a7ff2f8dab
Shelomanov <userbq201@mail.ru>
2018-02-06 16:54:25 +0300

    refactore answer for inline mode

--- src/features/inline-mode/generate-answer.js
+++ src/features/inline-mode/generate-answer.js
@@ -1,13 +1,13 @@
 const { anime: { captionInline } } = require('../../utils/text')
-
+const { baseURL } = require('../../config')
 
 module.exports = (list) => list.map((item) => (
   item = {
     id: item.id,
     type: 'photo',
     title: item.russian || item.name,
-    thumb_url: `https://shikimori.org/${item.image.x96}`,
-    photo_url: `https://shikimori.org/${item.image.original}`,
+    thumb_url: `${baseURL}/${item.image.x96}`,
+    photo_url: `${baseURL}/${item.image.original}`,
     caption: captionInline(item),
   }
 ))
